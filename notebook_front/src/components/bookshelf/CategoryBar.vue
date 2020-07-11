6e9d9e044b43ebdc490132e1510460544b96443e
Xindi-Tang <xindi.tang.0418@gmail.com>
2020-07-11 23:21:32 +0800

    frontend for deleting category after step11 by xindi

--- notebook_front/src/components/bookshelf/CategoryBar.vue
+++ notebook_front/src/components/bookshelf/CategoryBar.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <el-tabs v-model="currentCid" @tab-click="handleClick" class="categories-tabs">
+    <el-tabs type="card" v-model="currentCid" @tab-click="handleClick" class="categories-tabs" editable @edit="editCategory">
       <el-tab-pane v-for="(item,i) in categories" :label="item.name" :key="i" :name="item.id.toString()"></el-tab-pane>
     </el-tabs>
+    <category-edit-form ref="editForm" @update="update"></category-edit-form>
   </div>
 </template>
 
 <script>
+  import CategoryEditForm from "./CategoryEditForm";
     export default {
       name: "CategoryBar",
+      components:{CategoryEditForm},
       data(){
         return{
           currentCid:'',
@@ -33,21 +36,71 @@
       //     })
       // },
       mounted(){
-        var _this=this;
-        this.axios.get("/categories")
-          .then(function (response){
-            if(response.status===200){
-                      _this.categories=response.data
-                      _this.currentCid = _this.categories[0].id.toString();
-                      _this.$emit('categoryDefault')
-                      console.log(response)
-                    }
-          })
+        this.update()
       },
       methods: {
+        update(){
+          var _this=this;
+          this.axios.get("/categories")
+            .then(function (response){
+              if(response.status===200){
+                _this.categories=response.data
+                _this.currentCid = _this.categories[0].id.toString();
+                _this.$emit('categoryDefault')
+                console.log(response)
+              }
+            })
+        },
         handleClick(tab, event) {
           this.$emit('categorySelect')
-        }
+        },
+        editCategory(targetName,action) {
+          if (action === 'remove') {
+            var _this = this
+            this.$confirm('是否永久删除该笔记分类?', '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              _this.axios.get('category/delete/'+targetName.toString())
+                .then(function (response) {
+                  if(response.data.status === 200){
+                    _this.$message({
+                      type: 'warning',
+                      message: '删除成功!'
+                    });
+                    _this.update();
+                    // let tabs = _this.categories;
+                    // let activeName = _this.currentCid;
+                    // if (activeName === targetName) {
+                    //   tabs.forEach((tab, index) => {
+                    //     if (tab.id.toString() === targetName) {
+                    //       let nextTab = tabs[index + 1] || tabs[index - 1];
+                    //       if (nextTab) {
+                    //         activeName = nextTab.id.toString();
+                    //       }
+                    //     }
+                    //   });
+                    // }
+                    // _this.currentCid = activeName;
+                    // _this.categories = tabs.filter(tab => tab.id.toString() !== targetName);
+                  }
+                })
+                .catch(function (error) {
+                  console.log(error)
+                })
+            }).catch(() => {
+              this.$message({
+                type: 'info',
+                message: '已取消删除'
+              });
+            });
+          }
+          if(action === 'add'){
+            this.$refs.editForm.dialogFormVisible = true
+            this.$refs.editForm.isCreate = true
+          }
+        },
       }
     }
 </script>
@@ -58,5 +111,14 @@
     height: 30px !important;
     line-height: 20px !important;
     font-size: 13px;
+  }
+  .categories-tabs >>> .el-tabs__header {
+    margin: 5px 0px 10px 0px;
+    height: 30px !important;
+    line-height: 20px !important;
+    font-size: 13px;
+  }
+  .categories-tabs >>> .el-tabs__new-tab {
+    margin: 5px 0px !important;
   }
 </style>
---
+++ notebook_front/src/components/bookshelf/CategoryEditForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-dialog title="编辑笔记分类" :visible.sync="dialogFormVisible" :before-close="handleClose">
+      <el-form :model="form">
+        <el-form-item label="分类名" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="">取 消</el-button>
+        <el-button type="primary" @click="updateCategory">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "CategoryEditForm",
+      data(){
+        return {
+          isCreate: false,
+          dialogFormVisible: false,
+          formLabelWidth: '120px',
+          form:{
+            id:'',
+            name:'',
+          },
+        }
+      },
+      methods:{
+        handleClose(done) {
+          var _this=this;
+          this.$confirm('保存修改并关闭？')
+            .then(_ => {
+              _this.updateNote();
+            })
+            .catch(_ => {});
+        },
+        updateCategory(){}
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
